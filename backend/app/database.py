e18875a2e70a90283ca500f1e7890915e7ccd5de
Sagar <sagarphadkule@gmail.com>
2025-04-28 20:48:04 -0700

    2

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,13 +1,12 @@
 from sqlalchemy.orm import sessionmaker
 import sqlalchemy as sa
-from sqlalchemy.ext.declarative import declarative_base
+
 
 SA_DATABASE_URL = "sqlite:///./jeopardy.db"
 # SA_DATABASE_URL = "postgresql://mental:TODO@jeopardyze.xyz:5432/jeopardyze"
 
 engine = sa.create_engine(SA_DATABASE_URL)
 Session = sessionmaker(autocommit=False, autoflush=False, bind=engine) # Session Factory
-Base = declarative_base()
 
 def get_db():
     db_session = Session()
--- backend/app/models.py
+++ backend/app/models.py
@@ -1,2 +1,2 @@
-from .database import Base
-
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
