311ca609eb810af4b5223a40173d2bcf50fc9646
Sagar <sagarphadkule@gmail.com>
2025-05-02 16:12:40 -0700

    h2

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,12 +5,13 @@ import { ChakraProvider } from '@chakra-ui/react'
 
 import './index.css'
 import App from './App.tsx'
+import theme from './theme'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <App />
       </ChakraProvider>
     </BrowserRouter>
