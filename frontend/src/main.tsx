63526759281942e5ea123ed920e99168685bc992
Sagar <sagarphadkule@gmail.com>
2025-06-18 17:44:08 -0700

    login10

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,13 +6,16 @@ import { ChakraProvider } from '@chakra-ui/react'
 import './index.css'
 import App from './App.tsx'
 import theme from './theme'
+import { AuthProvider } from './contexts/AuthContext'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <ChakraProvider theme={theme}>
-        <App />
+        <AuthProvider>
+          <App />
+        </AuthProvider>
       </ChakraProvider>
     </BrowserRouter>
   </StrictMode>
